00d805470bc14b27a7d6049a76e8959f983c0ab8
Rkenfield <Ryanjkenfield@aol.com>
2020-04-05 13:30:22 -0600

    working on floating ai

--- NightmareHymn/Assets/Scripts/Enemies/floatingTurretEnemy.cs
+++ NightmareHymn/Assets/Scripts/Enemies/floatingTurretEnemy.cs
@@ -7,17 +7,20 @@ public class floatingTurretEnemy : BaseEnemy
     public float SafetyDistance;
     private Transform playerTransform;
     private float distance;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         health = 5;
         speed = 2;
         playerTransform = player.GetComponent<Transform>();
-    
+
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         distance = Vector3.Distance(playerTransform.position, transform.position);
 
@@ -25,11 +28,15 @@ public class floatingTurretEnemy : BaseEnemy
         //moving towards player
         if (distance > SafetyDistance)
         {
+            rb.constraints = ~RigidbodyConstraints.FreezePositionX | ~RigidbodyConstraints.FreezePositionY;
 
             transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
 
         }
-
+        else
+        {
+            rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY; 
+        }
     }
 
     void OnCollisionEnter(Collision collision)
