e911688adc129ffbd928f3c89c45aa83793e8e20
Rkenfield <Ryanjkenfield@aol.com>
2020-04-07 16:40:49 -0600

    player dying should be done

--- NightmareHymn/Assets/Scripts/Enemies/BaseEnemy.cs
+++ NightmareHymn/Assets/Scripts/Enemies/BaseEnemy.cs
@@ -15,7 +15,6 @@ public class BaseEnemy : MonoBehaviour
 
     void Start()
     {
-        
         startSpeed = speed;
     }
         
---
+++ NightmareHymn/Assets/Scripts/Enemies/CollideEnemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideEnemy : BaseEnemy
+{
+    private Transform playerTransform;
+    void Start()
+    {
+        playerTransform = player.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
