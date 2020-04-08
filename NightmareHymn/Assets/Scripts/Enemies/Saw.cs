fa489788466156147e95b17e52b55ecaba327618
Rkenfield <Ryanjkenfield@aol.com>
2020-04-08 17:19:37 -0600

    saw and falling platforms done

---
+++ NightmareHymn/Assets/Scripts/Enemies/Saw.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Saw : MonoBehaviour
+{
+    float count;
+
+    public float speed;
+    public float width;
+    public float height;
+
+    float startx, starty, startz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startx = transform.position.x;
+        starty = transform.position.y;
+        startz = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        count += Time.deltaTime * speed;
+
+        float x = startx + Mathf.Cos(count) * width;
+        float y = starty + Mathf.Sin(count) * height;
+        
+
+        transform.position = new Vector3(x, y, startz);
+    }
+
+}
---
+++ NightmareHymn/Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+    bool fall;
+    int count;
+    public int lifeSpan;
+
+    public Rigidbody rb;
+    public float gravity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 1;
+        fall = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fall == true)
+        {
+            rb.useGravity = true;
+       
+            if(count % lifeSpan == 0)
+            {
+                Destroy(gameObject);
+            }
+            count++;
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            fall = true;
+        }
+    }
+}
