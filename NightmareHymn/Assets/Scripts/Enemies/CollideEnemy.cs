1b1a0d12a3de99e83c45083382090d3433cc1e11
OdinsWrath <mtlhead93@gmail.com>
2020-04-23 13:48:37 -0600

    Fixed Chase Enemy AI

--- NightmareHymn/Assets/Scripts/Enemies/CollideEnemy.cs
+++ NightmareHymn/Assets/Scripts/Enemies/CollideEnemy.cs
@@ -6,6 +6,8 @@ public class CollideEnemy : BaseEnemy
 {
     private Transform playerTransform;
     public Rigidbody rb;
+    private float LorR = 0;
+    public float chaseSpeed = 100;
 
     void Start()
     {
@@ -14,8 +16,24 @@ public class CollideEnemy : BaseEnemy
         rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
+        LorR = playerTransform.position.x - transform.position.x;
+        
+        if (LorR > 0)
+        {
+            LorR = 1f;
+        }
+        else if (LorR < 0)
+        {
+            LorR = -1f;
+        }
+        else
+        {
+            Debug.Log("Chasing Enemy: Left or Right set to 0");
+        }
+
+        //rb.velocity = new Vector3((LorR * chaseSpeed) * Time.deltaTime, rb.velocity.y, rb.velocity.z);
+        rb.AddForce(new Vector3((LorR * chaseSpeed) * Time.deltaTime, rb.velocity.y, rb.velocity.z), ForceMode.Impulse);
     }
 }
