b2f0df1ed06049eedf75059265faeada56a05fbf
Rkenfield <Ryanjkenfield@aol.com>
2020-04-23 12:58:37 -0600

    charge enemy

--- NightmareHymn/Assets/Scripts/Enemies/CollideEnemy.cs
+++ NightmareHymn/Assets/Scripts/Enemies/CollideEnemy.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class CollideEnemy : BaseEnemy
 {
     private Transform playerTransform;
+    public Rigidbody rb;
+
     void Start()
     {
         playerTransform = player.GetComponent<Transform>();
+        health = 5;
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, speed * Time.deltaTime);
     }
 }
